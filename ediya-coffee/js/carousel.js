605e3bde0b416b4d7ebb92f32251b16a5759f2b5
soowan kim <57820023+wan-n@users.noreply.github.com>
2022-06-01 23:25:07 +0900

    carousel 코드짜기 완료

--- ediya-coffee/js/carousel.js
+++ ediya-coffee/js/carousel.js
@@ -5,6 +5,7 @@ const slideCount = slideImg.length; // 슬라이드 개수
 const prev = document.querySelector('.prev'); //이전 버튼
 const next = document.querySelector('.next'); //다음 버튼
 const slideWidth = 300; //한개의 슬라이드 넓이
+const slideMargin = 0; //슬라이드간의 margin 값
 
 //전체 슬라이드 컨테이너 넓이 설정
 slides.style.width = (slideWidth + slideMargin) * slideCount + 'px';
@@ -16,17 +17,17 @@ function moveSlide(num) {
 }
 
 //슬라이드가 넘어가는 시간(0.5s) 뒤에 효과 스킵하고 바로 처음, 또는 마지막 이미지로 넘긴다.
-function skipEffect(num){
+function skipEffect(){
     if(currentIdx == 0){
         setTimeout(function () {
-            slides.style.left = -6 * 300 + 'px';
-            currentIdx = 6;
+            slides.style.left = -(currentIdx + 6) * 300 + 'px';
+            currentIdx = currentIdx + 6;
             slides.style.transition = `${0}s`;
           }, 500);
     }else if(currentIdx == 7){
         setTimeout(function () {
-            slides.style.left = -1 * 300 + 'px';
-            currentIdx = 1;
+            slides.style.left = -(currentIdx - 6) * 300 + 'px';
+            currentIdx = currentIdx - 6;
             slides.style.transition = `${0}s`;
           }, 500);
     }
@@ -38,7 +39,7 @@ prev.addEventListener('click', function () {
         moveSlide(currentIdx - 1); 
     }else if(currentIdx <= 1){
         moveSlide(currentIdx - 1);
-        skipEffect(currentIdx);
+        skipEffect();
     }
   
 });
@@ -48,6 +49,6 @@ next.addEventListener('click', function () {
         moveSlide(currentIdx + 1);
     }else if(currentIdx >= 6){
         moveSlide(currentIdx + 1);
-        skipEffect(currentIdx);
+        skipEffect();
     }
 });
